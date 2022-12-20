6c2b61822205a09113f0e86d5012aa45c6395f46
Daniel Toledo <dan992965@gmail.com>
2022-12-20 16:18:58 -0300

    v1.2

--- src/components/Default.tsx
+++ src/components/Default.tsx
@@ -1,18 +1,24 @@
-import Logo from '/logo/logofinal.png'
-import './Default.css'
+import Logo from "/logo/logofinal.png";
+import "./Default.css";
 
 const Default = () => {
   return (
-    <div className='default'>
-        <div className='navbar'>
-            <img className='logo' src={Logo} alt="" />
-            <ul className='navOptions'>
-              <li><a href="">Detalhes</a></li>
-              <li><a href="" className='contactNav'>Contato</a></li>
-            </ul>
-        </div>
+    <div className="default">
+      <div className="navbar">
+        <img className="logo" src={Logo} alt="" />
+        <ul className="navOptions">
+          <li>
+            <a href="">Serviços</a>
+          </li>
+          <li>
+            <a href="" className="contactNav">
+              Contato
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Default
+export default Default;
--- src/components/Value.tsx
+++ src/components/Value.tsx
@@ -1,6 +1,6 @@
 import "./Value.css";
 import Cards from './Cards'
-import { FcIdea, FcSurvey, FcLibrary, FcTodoList } from "react-icons/fc";
+import { FcIdea, FcSurvey, FcLibrary, FcHome } from "react-icons/fc";
 import BgValue from "/Background.png";
 
 const Value = () => {
@@ -15,24 +15,24 @@ const Value = () => {
         </div>
         <div className="floatRight">
           <Cards
+            title="Casas e Terrenos"
+            subTitle="Aqui na nossa imobiliária temos uma grande variedade de casas e terrenos à venda. Temos casas em todos os tamanhos e em todas as localizações, desde apartamentos modernos em zonas residenciais a casas de campo localizadas em áreas rurais."
+            icon={<FcHome size={100} />}
+          />
+          <Cards
             title="Projetos Arquitetônicos"
-            subTitle="Criação de um documento que especifica as características da casa, incluindo design, layout, materiais e outros detalhes construtivos."
+            subTitle="Projetos elaborados por profissionais qualificados para projetar e construir uma casa exclusiva. Estes projetos incluem a criação de fluxos de trabalho, desenho de plantas, criação de orçamentos, seleção de materiais e execução de construção."
             icon={<FcIdea size={100} />}
           />
           <Cards
             title="Construção"
-            subTitle="Realizamos o processo de projetar e construir uma estrutura que seja apropriada para uso residencial. É um trabalho que exige planejamento, recursos e habilidades específicas."
+            subTitle="Processo de projetar e construir uma casa, desde a fase de planejamento até a conclusão do projeto. O processo inclui a localização adequada, a seleção de materiais e a seleção de métodos de construção, bem como a escolha de fornecedores e contratados."
             icon={<FcLibrary size={100} />}
           />
           <Cards
-            title="Gerenciamento de Obras"
-            subTitle="Criação de um documento que especifica as características da casa, incluindo design, layout, materiais e outros detalhes construtivos."
+            title="Regularização de Obras"
+            subTitle="Processo de regularização das obras realizadas em uma residência, a fim de garantir que todos os trabalhos e serviços realizados sejam de acordo com as leis vigentes, bem como que não coloquem em risco a segurança, integridade e saúde das pessoas que frequentam o imóvel."
             icon={<FcSurvey size={100} />}
-          />
-          <Cards
-            title="Projetos Arquitetônicos"
-            subTitle="Criação de um documento que especifica as características da casa, incluindo design, layout, materiais e outros detalhes construtivos."
-            icon={<FcTodoList size={100} />}
           />
         </div>
       </div>
