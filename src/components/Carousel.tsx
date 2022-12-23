9cf34c1109f43176e2edb49bd7e1bb562832ce20
Daniel Toledo <dan992965@gmail.com>
2022-12-23 20:18:47 -0300

    v2.0

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,16 +2,16 @@ import Slider from 'react-slick';
 import './Carousel.css'
 
 const images = [
-  { name: "img1", url: "src/assets/slideImg/img1.jpg" },
-  { name: "img2", url: "src/assets/slideImg/img2.jpg" },
-  { name: "img3", url: "src/assets/slideImg/img3.jpg" },
-  { name: "img4", url: "src/assets/slideImg/img4.jpg" },
-  { name: "img5", url: "src/assets/slideImg/img5.jpg" },
-  { name: "img6", url: "src/assets/slideImg/img6.jpg" },
-  { name: "img7", url: "src/assets/slideImg/img7.jpg" },
-  { name: "img8", url: "src/assets/slideImg/img8.jpg" },
-  { name: "img9", url: "src/assets/slideImg/img9.jpg" },
-  { name: "img10", url: "src/assets/slideImg/img10.jpg" },
+  { name: "img1", url: "./src/assets/slideImg/img1.jpg" },
+  { name: "img2", url: "./src/assets/slideImg/img2.jpg" },
+  { name: "img3", url: "./src/assets/slideImg/img3.jpg" },
+  { name: "img4", url: "./src/assets/slideImg/img4.jpg" },
+  { name: "img5", url: "./src/assets/slideImg/img5.jpg" },
+  { name: "img6", url: "./src/assets/slideImg/img6.jpg" },
+  { name: "img7", url: "./src/assets/slideImg/img7.jpg" },
+  { name: "img8", url: "./src/assets/slideImg/img8.jpg" },
+  { name: "img9", url: "./src/assets/slideImg/img9.jpg" },
+  { name: "img10", url: "./src/assets/slideImg/img10.jpg" },
 ];
 
 export default function Carousel() {
